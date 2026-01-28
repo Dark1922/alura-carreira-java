fad055d27cb86b11867d7c6e72ef854d493dd3e6
João Victor Martins de Matos <joao.matos@solutis.com.br>
2026-01-28 05:26:15 -0300

    Trabalhando com textos

--- primeiroprojeto/src/Main.java
+++ primeiroprojeto/src/Main.java
@@ -8,5 +8,18 @@ public class Main {
         boolean incluirNoPlano = true;
         double notaFilme = 8.2;
 
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        System.out.println(media);
+
+        String sinopse;
+        sinopse = "Filme de aventura com galâ dos ano 80";
+        System.out.println(sinopse);
+
+        sinopse = """
+                Filme de aventura
+                Com galâ 
+                Dos ano 80
+                """;
+        System.out.println(sinopse);
     }
 }
