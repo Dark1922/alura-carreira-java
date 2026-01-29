daf889e6f42dfe37541d6f399f6e8cf4d8eba798
João Victor Martins de Matos <joao.matos@solutis.com.br>
2026-01-29 12:26:53 -0300

    Avaliando o filme

--- primeiroprojeto/src/Loop.java
+++ primeiroprojeto/src/Loop.java
@@ -14,5 +14,15 @@ public class Loop {
 
         System.out.println("Média de avaliações " + mediaAvaliacao/3);
 
+//        while (nota != -1) {
+//            System.out.println("Diga sua avaliação para o filme ou -1 para encerrar  ");
+//            nota = leitura.nextDouble();
+//
+//            if (nota != -1) {
+//                mediaAvaliacao +=  nota;
+//                totalDeNotas++;
+//            }
+//
+//        }
     }
 }
